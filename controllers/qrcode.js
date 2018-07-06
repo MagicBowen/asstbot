1f82e674c75d9d1a5219f4625633ce04d989a644
magicbowen <e.bowen.wang@icloud.com>
2018-07-06 17:55:15 +0800

    fix error

--- controllers/qrcode.js
+++ controllers/qrcode.js
@@ -6,10 +6,9 @@ const logger = require('../utils/logger').logger('controller_qrcode');
 
 async function getQrCode(ctx) {
     try {
-        // const url = 'https://api.weixin.qq.com/wxa/getwxacode?access_token=';
-        // const tocken = '11_zIacgPwEIDkMaARLMe2b7zyaa8a2BFhc9fqft4ZUqFxJsOM0v6ONxa0niu0tNs4tBNSPxpe8nIgn68ZkgU1MLoOlgHazn8ZvNHdpGpbFeymodOiDag2vkAjBEiECQLbADAXLG';
         const tocken = await accessTocken.getTocken();
-        const url = 'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=' + tocken;
+        const url = 'https://api.weixin.qq.com/wxa/getwxacode?access_token=' + tocken;
+        // const url = 'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=' + tocken;
 
         const filename = uuid.v1() + '.png';
         const filePath = 'static/image/' + filename;
@@ -18,8 +17,9 @@ async function getQrCode(ctx) {
             url: url,
             method: "POST",
             json: {
-                scene: ctx.query.scene,
-                page : 'pages/home/main' // ctx.query.page
+                // scene: ctx.query.scene,
+                // page : 'pages/index/main'
+                path : 'pages/index?id=' + ctx.query.scene
             }
         }).pipe(fs.createWriteStream(filePath));
 
