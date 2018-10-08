692723635a88da30ba7fec626165a105e155d26d
magicbowen <e.bowen.wang@icloud.com>
2018-10-08 15:09:13 +0800

    fix bug

--- controllers/chatbot.js
+++ controllers/chatbot.js
@@ -99,7 +99,7 @@ const talkToChatBot = async (agent, userId, type, params) => {
         result =  await chatbot.replyToEvent(user, params.event, params);
     }
     result = await chatbot.replyToEvent(user, type, params);
-    return addTtsForMsgs(result)
+    return await addTtsForMsgs(result)
 }
 
 const handleMessage = async (ctx, agent) => {
