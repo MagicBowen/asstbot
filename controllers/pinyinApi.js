49e2a2c421e30867ffd160ee6956e88214cc19a4
WeiGangqiang <284894567@qq.com>
2018-10-31 20:33:33 +0800

    fix bug

--- controllers/pinyinApi.js
+++ controllers/pinyinApi.js
@@ -4,10 +4,12 @@ var pinyin = require("pinyin");
 
 const getPinyin = async(ctx) => {
     const sentence = ctx.request.query.sentence;
-    var result  = pinyin(sentence, {heteronym: true})
+    var ret = {}
+    ret.wordPinyin = pinyin(sentence, {heteronym: true})
+    ret.termPinyin = pinyin(sentence, {heteronym: true, segment: true})
     ctx.response.type = "application/json";
     ctx.response.status = 200;
-    ctx.response.body = {result : 'success', data : result};
+    ctx.response.body = {result : 'success', data : ret};
 }
 
 
