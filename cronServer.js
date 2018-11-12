2d0797e8ae52c0feea5b04e4413afc09205c5c20
WeiGangqiang <284894567@qq.com>
2018-11-12 14:34:10 +0800

    add cron Server.js

---
+++ cronServer.js
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////
+const schedule = require('node-schedule');
+const model = require('./models/model');
+model.init();
+
+const  scheduleCronstyle = ()=>{
+  //每分钟的第30秒定时执行一次:
+    schedule.scheduleJob('30 * * * * *',()=>{
+        console.log('scheduleCronstyle:' + new Date());
+    }); 
+}
+
+scheduleCronstyle();
