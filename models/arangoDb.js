7677ebc3cf618e22e9d115c3b298031272ddee86
WeiGangqiang <284894567@qq.com>
2018-11-12 11:40:32 +0800

    refact code

---
+++ models/arangoDb.js
@@ -0,0 +1,20 @@
+var arango = require('arangojs');
+var config = require('../config')
+const logger = require('../utils/logger').logger('arangoDb');
+var db = null 
+
+function getDb(){
+    logger.info("read config ", JSON.stringify(config))
+    if(db == null){
+        Database = arango.Database;
+        db = new Database(`http://${config.arangoHost}:${config.arangoPort}`);
+        db.useDatabase('waterDrop');
+        db.useBasicAuth(config.arangoUser, config.arangoPassword)
+        logger.info("arango db init success")
+    }
+    return db
+}
+
+module.exports={
+    getDb
+}
